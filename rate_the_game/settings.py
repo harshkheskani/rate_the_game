1cdd5d805a086dafe44d76a45510f165d90c5a55
Harvey Russell <2461525r@student.gla.ac.uk>
2021-03-12 12:11:57 +0000

    created django app rate_the_game_app and added it to INSTALLED_APPS in settings.py

--- rate_the_game/settings.py
+++ rate_the_game/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rate_the_game_app',
 ]
 
 MIDDLEWARE = [
---
+++ rate_the_game_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class RateTheGameAppConfig(AppConfig):
+    name = 'rate_the_game_app'
