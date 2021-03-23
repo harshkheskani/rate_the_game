c9e0baff98b501d3517a17b24e554b78dc2556b7
Harvey Russell <2461525r@student.gla.ac.uk>
2021-03-23 15:29:49 +0000

    added static and media folders and set up settings.py and rate_the_game/urls.py so they will work

--- rate_the_game/settings.py
+++ rate_the_game/settings.py
@@ -15,6 +15,8 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -63,6 +65,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -114,8 +117,11 @@ USE_L10N = True
 
 USE_TZ = True
 
+#Media stuff
+MEDIA_ROOT = MEDIA_DIR
+MEDIA_URL = '/media/'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
+STATICFILES_DIRS = [STATIC_DIR,]
 STATIC_URL = '/static/'
--- rate_the_game/urls.py
+++ rate_the_game/urls.py
@@ -17,8 +17,10 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import include
 from rate_the_game_app import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('rate_the_game_app/', include('rate_the_game_app.urls')),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
