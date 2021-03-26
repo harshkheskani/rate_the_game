e3720e52660f67a1cc5d064de113eb3ef260eae4
Harvey Russell <2461525r@student.gla.ac.uk>
2021-03-26 13:12:36 +0000

    added links for base buttons (not all just top right ones im doing user authentication with) ,created a template tags folder with blank files but not using it just yet

---
+++ rate_the_game_app/templatetags/rate_the_game_app_template_tags.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 26 12:57:58 2021
+
+@author: Harvey
+"""
+
+from django import template
--- rate_the_game_app/urls.py
+++ rate_the_game_app/urls.py
@@ -11,6 +11,7 @@ from rate_the_game_app import views
 app_name = 'rate_the_game_app'
 
 urlpatterns = [
+        path('', views.index, name='index'),
         path('register/', views.register, name='register'),
         path('login/', views.user_login, name='login'),
         path('logout/', views.user_logout, name='logout'),
